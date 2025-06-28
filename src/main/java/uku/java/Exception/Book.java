0809761341b47306cf1a5ddbcd5c0be0243dc9f4
Veta Ibragimova <i.firiat@gmail.com>
2025-06-28 20:32:01 +0300

    exception

---
+++ src/main/java/uku/java/Exception/Book.java
@@ -0,0 +1,58 @@
+package uku.java.Exception;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private int availableCopies;
+
+    public Book(String title, String author, int availableCopies) {
+        this.title = title;
+        this.author = author;
+        this.availableCopies = availableCopies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book: title = %s, author = %s, availableCopies = %s", title, author, availableCopies);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return Objects.equals(book.title, title) &&
+                Objects.equals(book.author, author);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Objects.hash(title, author);
+    }
+}
